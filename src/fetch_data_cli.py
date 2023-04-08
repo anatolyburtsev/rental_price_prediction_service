526b992422d9f3f24d708af02a946e03f82120af
Anatolii Burtsev <aburtsev@amazon.com>
2023-04-08 12:38:55 -0700

    replace argsparse with click

--- src/fetch_data_cli.py
+++ src/fetch_data_cli.py
@@ -1,35 +1,39 @@
 import logging
-import argparse
+import click
 
 from io_lib import store_json_to_csv_local_file, dummy_store_data, store_json_to_csv_s3
 from zumper_lib import get_all_listings
 
 logger = logging.getLogger()
 
-# TODO: replace choices with enums
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--city", help="city to fetch data about", default="vancouver-bc")
-    parser.add_argument("-o", "--output_prefix", help="output filename prefix. It will be suffixed with current time",
-                        default="output")
-    parser.add_argument("-d", "--destination", help="where save file to", default="local", choices=["local", "s3"])
-    parser.add_argument("-f", "--output_format", help="output format", default="csv", choices=["csv", "dummy"])
-    parser.add_argument("-s3", "--s3_path", help="s3 path to save data to. Mandatory if destination is s3")
-    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-    parser.add_argument("-fpo", "--first_page_only", help="download just first page of listings", default=False,
-                        action="store_true")
 
-    args = parser.parse_args()
-
-    if args.verbose:
+@click.command()
+@click.option("--city", help="city to fetch data about (default: vancouver-bc)", default="vancouver-bc")
+@click.option("-o", "--output_prefix",
+              help="output filename prefix. It will be suffixed with current time (default: output)",
+              default="output")
+@click.option("-d", "--destination", help="where save file to (default: local)", default="local",
+              type=click.Choice(["local", "s3"]))
+@click.option("-f", "--output_format", help="output format (default: csv)", default="csv",
+              type=click.Choice(["csv", "dummy"]))
+@click.option("-s3", "--s3_path", help="s3 path to save data to. Mandatory if destination is s3")
+@click.option("-v", "--verbose", help="increase output verbosity", is_flag=True)
+@click.option("-fpo", "--first_page_only", help="download just first page of listings (default: False)", default=False,
+              is_flag=True)
+def main(city, output_prefix, destination, output_format, s3_path, verbose, first_page_only):
+    if verbose:
         raise Exception("To be implemented")
 
-    all_data = get_all_listings(args.city, args.first_page_only)
-    if args.output_format == "dummy":
+    all_data = get_all_listings(city, first_page_only)
+    if output_format == "dummy":
         dummy_store_data(all_data)
-    elif args.destination == "local":
-        store_json_to_csv_local_file(all_data, filename_prefix=f"{args.output_prefix}_{args.city}")
-    elif args.destination == "s3":
-        if not args.s3_path:
+    elif destination == "local":
+        store_json_to_csv_local_file(all_data, filename_prefix=f"{output_prefix}_{city}")
+    elif destination == "s3":
+        if not s3_path:
             raise Exception("s3_path must be specified if destination is s3")
-        store_json_to_csv_s3(all_data, args.s3_path, filename_prefix=f"{args.output_prefix}_{args.city}")
+        store_json_to_csv_s3(all_data, s3_path, filename_prefix=f"{output_prefix}_{city}")
+
+
+if __name__ == '__main__':
+    main()
